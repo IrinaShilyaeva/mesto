7aa27f60dd5a33174ad686dc1bea57f469604323
Irina Shilyaeva <shilyaeva.irinka@yandex.ru>
2021-11-12 17:47:04 +0300

    fixed: переделать ошибки ревьюэра

--- script/index.js
+++ script/index.js
@@ -2,6 +2,10 @@ let popupElement = document.querySelector('.popup');
 let closeButton = document.querySelector('.popup__close-button');
 let editButton = document.querySelector('.profile__edit-button');
 let formElement = popupElement.querySelector('.popup__form');
+let nameInput = document.querySelector('.popup__input_user_name');
+let infoInput = document.querySelector('.popup__input_user_info');
+let userName= document.querySelector('.profile__name');
+let userText= document.querySelector('.profile__text');
 
 function openPopup() {
   popupElement.classList.add('popup_opened');
@@ -13,12 +17,7 @@ function closePopup() {
 
 function formSubmitHandler (evt) {
   evt.preventDefault(); 
-  
-  let nameInput = document.querySelector('.popup__name');
-  let infoInput = document.querySelector('.popup__info');
-  let userName= document.querySelector('.profile__name');
-  let userText= document.querySelector('.profile__text');
-
+   
   userName.textContent = nameInput.value;
   userText.textContent = infoInput.value;
   popupElement.classList.remove('popup_opened');
